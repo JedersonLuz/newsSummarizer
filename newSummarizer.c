efcc84664f9d73d10c14606b1b04e5fb53bb7ae8
Jederson_Luz <jedersonalpha@gmail.com>
2017-11-24 10:28:32 -0300

    Tá pronto o bagulho

--- newSummarizer.c
+++ newSummarizer.c
@@ -4,26 +4,84 @@
 #include "in_out.h"
 
 typedef struct {
-  char *word;
+  char word[20];
   int frequency;
-}word;
+}Word;
 
-char ** selectImportantWords (char **words, int size) {
+Word * selectImportantWords (char **words, int size);
+
+void stringLwr(char *s);
+
+void realocChar (char **words, int size);
+
+void removeSpecialChar (char **words, int size);
+
+char ** recoveryData (char **words, char *path, int size);
+
+int main(int argc, char const *argv[]) {
+  char **words;
+  Word *importantWords = malloc(sizeof(Word));
   int i;
-  for (i = 0; i < size; i++) {
 
+  //int option = str4int(input("Informe o tipo de conteúdo (1-website, 2-arquivo de texto puro): "));
+  char *path = input("Informe o caminho para o website ou arquivo de texto: ");
+  int size = str4int(input("Informe o tamanho do resumo (a quantidade de palavras): "));
+
+  words = recoveryData(words, path, size);
+
+  removeSpecialChar(words, size);
+
+  importantWords = selectImportantWords(words, size);
+
+
+  return 0;
+}
+
+Word * selectImportantWords (char **words, int size) {
+  int i, y, flag, countWords = 0, k;
+  Word *importantWords = malloc(sizeof(Word)*200);
+  char **stopWords;
+  stopWords = recoveryData(stopWords, "stopWords.txt", 81);
+
+  for (i = 0; i < size; i++) {
+    //printf("%d\n", i);
+    for (y = 0; y < 81; y++) {
+      flag = strcmp(words[i], stopWords[y]);
+      if (flag == 0){
+        break;
+      }
+    }
+    if(flag != 0){
+      int existe = 0;
+        for(k=0; k<countWords; k++){
+          if((strcmp(importantWords[k].word,words[i]))==0){
+            importantWords[k].frequency++;
+            existe = 1;
+            break;
+          }
+        }
+        if(existe==0){
+          strcpy(importantWords[countWords].word,words[i]);
+          importantWords[countWords].frequency = 1;
+          countWords++;
+        }
+      }
+    }
+
+  for (i = 0; i < countWords-1; i++) {
+    printf("%s | %d\n", importantWords[i].word, importantWords[i].frequency);
   }
-  return words;
+  return importantWords;
 }
 
 void stringLwr(char *s) {
-    int i=0;
-    while(s[i]!='\0') {
-        if(s[i]>='A' && s[i]<='Z') {
-            s[i]=s[i]+32;
-        }
-        ++i;
+  int i=0;
+  while(s[i]!='\0') {
+    if(s[i]>='A' && s[i]<='Z') {
+      s[i]=s[i]+32;
     }
+    ++i;
+  }
 }
 
 // Realoca os caracteres especiais que abre frases, tais como parenteses e aspas simples e duplas
@@ -35,7 +93,7 @@ void realocChar (char **words, int size) {
         for (j = 0; j < 30; j++) {
           words[i][j] = words[i][j+1];
           if (words[i][j] == '"')
-            words[i][j] = '\0';
+          words[i][j] = '\0';
         }
       }
     }
@@ -49,13 +107,13 @@ void removeSpecialChar (char **words, int size) {
     for (y = 0; y < 30; y++) {
       if ((words[i][y] == '\n') || (words[i][y] == ':') || (words[i][y] == '.') || (words[i][y] == ',') || (words[i][y] == ';') || (words[i][y] == '!') || (words[i][y] == '?') || (words[i][y] == ')')) {
         if (!(((words[i][y-1] >= 48) && (words[i][y-1] <= 57)) && ((words[i][y+1] >= 48) && (words[i][y+1] <= 57))))
-          words[i][y] = '\0';
+        words[i][y] = '\0';
 
         if (words[i][y] != ',')
-          words[i][y] = '\0';
+        words[i][y] = '\0';
 
         if (words[i][y] == '\0')
-          break;
+        break;
       }
     }
   }
@@ -81,28 +139,4 @@ char ** recoveryData (char **words, char *path, int size) {
   printf("Operação concluída com sucesso!\n");
 
   return words;
-}
-
-int main(int argc, char const *argv[]) {
-  char **words, **importantWords;
-  int i;
-
-  //int option = str4int(input("Informe o tipo de conteúdo (1-website, 2-arquivo de texto puro): "));
-  char *path = input("Informe o caminho para o website ou arquivo de texto: ");
-  int size = str4int(input("Informe o tamanho do resumo (a quantidade de palavras): "));
-
-  words = recoveryData(words, path, size);
-
-  importantWords = malloc(size * sizeof(words));
-  for (i = 0; i < size; i++){
-    importantWords[i] = malloc(sizeof(30));
-  }
-
-  removeSpecialChar(words, size);
-
-  for (i = 0; i < size; i++) {
-    printf("%s\n", words[i]);
-  }
-
-  return 0;
 }
